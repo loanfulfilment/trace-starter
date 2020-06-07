273c488db3bb1d82f2620dd35193903136c967cd
Swapnil Sankla <swapnil.sankla@gmail.com>
2020-06-07 19:52:22 +0530

    Add helper method fromKafkaHeaders

--- src/main/kotlin/com/swapnilsankla/tracestarter/TraceIdExtractor.kt
+++ src/main/kotlin/com/swapnilsankla/tracestarter/TraceIdExtractor.kt
@@ -1,6 +1,7 @@
 package com.swapnilsankla.tracestarter
 
 import org.springframework.stereotype.Component
+import org.apache.kafka.common.header.Headers
 
 @Component
 class TraceIdExtractor {
@@ -11,5 +12,11 @@ class TraceIdExtractor {
         return Trace(
             key = traceHeader,
             value = headers[traceHeader] as? ByteArray ?: ByteArray(1))
+    }
+
+    fun fromKafkaHeaders(headers: Headers): Trace {
+        return Trace(
+                key = traceHeader,
+                value = headers.headers(traceHeader).first().value())
     }
 }
